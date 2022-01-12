8e5598bb361973106d1f16cc337b1407a95d7ca2
Karolinaslp <slempkarolina@gmail.com>
2022-01-12 23:56:07 +0100

    Add task 0606 Conversion int to char.

---
+++ src/task0606_integer_to_char/Solution.java
@@ -0,0 +1,31 @@
+package task0606_integer_to_char;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Integer to digits;
+*/
+
+public class Solution {
+
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(reader.readLine());
+
+        String number = String.valueOf(num);
+        for (int i = 0; i < number.length(); i++) {
+            int j = Character.digit(number.charAt(i), 10);
+            if (j % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+        System.out.println("Even: " + even + " Odd: " + odd);
+    }
+}
