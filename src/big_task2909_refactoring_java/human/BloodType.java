1172347bc65d0e9eb3c07b01f4ba8a30b8fbd5e0
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-14 19:02:08 +0200

    Create BloodType class

---
+++ src/big_task2909_refactoring_java/human/BloodType.java
@@ -0,0 +1,30 @@
+package big_task2909_refactoring_java.human;
+
+public class BloodType {
+    private final int code;
+
+        private BloodType(int code) {
+            this.code = code;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public static BloodType first() {
+            return new BloodType(1);
+        }
+
+        public static BloodType second() {
+            return new BloodType(2);
+        }
+
+        public static BloodType third() {
+            return new BloodType(3);
+        }
+
+        public static BloodType fourth() {
+            return new BloodType(4);
+        }
+    }
+
