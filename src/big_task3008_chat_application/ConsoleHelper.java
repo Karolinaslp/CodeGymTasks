6601c769b54ef57e82db8db3d5c81a0a8f1e15bb
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-19 17:32:49 +0200

    add static methods and field

--- src/big_task3008_chat_application/ConsoleHelper.java
+++ src/big_task3008_chat_application/ConsoleHelper.java
@@ -1,4 +1,40 @@
 package big_task3008_chat_application;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 public class ConsoleHelper {
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void writeMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static String readString() throws IOException {
+        String string = "";
+        while (true) {
+            try {
+                string = reader.readLine();
+                if (string != null) {
+                    return string;
+                }
+            } catch (IOException e) {
+                System.out.println("An error occurred while trying to enter text. Try again");
+            }
+        }
+    }
+
+    public static int readInt() throws IOException {
+        int number = 0;
+        while (true) {
+            try {
+                number = Integer.parseInt(Objects.requireNonNull(readString()));
+                return number;
+            } catch (NumberFormatException e) {
+                System.out.println("An error while trying to enter a number. Try again");
+            }
+        }
+    }
 }
