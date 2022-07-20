0d8972f1c4b5a7fcaf4e2d022a07a18e4a16e9dd
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-20 16:53:04 +0200

    implement Message class

--- src/big_task3008_chat_application/Message.java
+++ src/big_task3008_chat_application/Message.java
@@ -1,4 +1,27 @@
 package big_task3008_chat_application;
 
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable {
+    private final MessageType type;
+    private final String data;
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    public MessageType getType() {
+        return type;
+
+    }
+
+    public String getData() {
+        return data;
+    }
 }
