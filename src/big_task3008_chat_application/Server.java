3fe758df2beea6da811cd8b8733488c7e9c79a23
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-21 11:31:14 +0200

    add nested Handler class, implement main method in Server class

--- src/big_task3008_chat_application/Server.java
+++ src/big_task3008_chat_application/Server.java
@@ -1,7 +1,33 @@
 package big_task3008_chat_application;
 
-public class Server {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
 
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+        ConsoleHelper.writeMessage("Enter server port:");
+        final int port = ConsoleHelper.readInt();
+
+
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Chat server is running");
+            while (true) {
+                Socket socket = serverSocket.accept();
+                new Handler(socket).start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage("An error occurred while starting or running the server.");
+
+        }
+    }
+
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
     }
 }
