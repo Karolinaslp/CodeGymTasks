e3395084b6db49c48db07a49ff9e0c5bd5a78e9a
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-21 10:01:41 +0200

    set up socket connection

--- src/big_task3008_chat_application/Connection.java
+++ src/big_task3008_chat_application/Connection.java
@@ -1,4 +1,44 @@
 package big_task3008_chat_application;
 
-public class Connection {
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+public class Connection implements Closeable {
+    private final Socket socket;
+    private final ObjectOutputStream out;
+    private final ObjectInputStream in;
+
+    public Connection(Socket socket) throws IOException {
+        this.socket = socket;
+        out = new ObjectOutputStream(socket.getOutputStream());
+        in = new ObjectInputStream(socket.getInputStream());
+    }
+
+    @Override
+    public void close() throws IOException {
+        in.close();
+        out.close();
+        socket.close();
+    }
+
+    public void send(Message message) throws IOException {
+        synchronized (out) {
+            out.writeObject(message);
+        }
+    }
+
+    public Message receive() throws IOException, ClassNotFoundException {
+        Message message;
+        synchronized (in) {
+            message = (Message) in.readObject();
+        }
+        return message;
+    }
+
+    public SocketAddress getRemoteSocketAddress() {
+        return socket.getRemoteSocketAddress();
+    }
+
 }
+
