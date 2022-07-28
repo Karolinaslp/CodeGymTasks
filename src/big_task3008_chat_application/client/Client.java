20fd77048f08a5ae207f8ab0c07f888b174a3ee0
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-28 15:20:40 +0200

    add methods to Client class

--- src/big_task3008_chat_application/client/Client.java
+++ src/big_task3008_chat_application/client/Client.java
@@ -1,6 +1,12 @@
 package big_task3008_chat_application.client;
 
 import big_task3008_chat_application.Connection;
+import big_task3008_chat_application.ConsoleHelper;
+import big_task3008_chat_application.Message;
+import big_task3008_chat_application.MessageType;
+
+import java.io.IOException;
+
 
 public class Client {
     protected Connection connection;
@@ -8,5 +14,37 @@ public class Client {
 
     public class SocketThread extends Thread {
 
+    }
+
+    protected String getServerAddress() throws IOException {
+        System.out.println("Please, enter server address:");
+        return ConsoleHelper.readString();
+    }
+
+    protected int getServerPort() throws IOException {
+        System.out.println("Please, enter server port:");
+        return ConsoleHelper.readInt();
+    }
+
+    protected String getUserName() throws IOException {
+        System.out.println("Please, enter user name:");
+        return ConsoleHelper.readString();
+    }
+
+    protected boolean shouldSendTextFromConsole() {
+        return true;
+    }
+
+    protected SocketThread getSocketThread() {
+        return new SocketThread();
+    }
+
+    protected void sendTextMessage(String text) {
+        try {
+            connection.send(new Message(MessageType.TEXT, text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("Unable to send massage");
+            clientConnected = false;
+        }
     }
 }
