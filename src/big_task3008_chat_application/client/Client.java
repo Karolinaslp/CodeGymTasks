c73090cf154469b10a82588f8ade3e7d7b17da98
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-28 14:41:48 +0200

    create client package

---
+++ src/big_task3008_chat_application/client/Client.java
@@ -0,0 +1,12 @@
+package big_task3008_chat_application.client;
+
+import big_task3008_chat_application.Connection;
+
+public class Client {
+    protected Connection connection;
+    private volatile boolean clientConnected = false;
+
+    public class SocketThread extends Thread {
+
+    }
+}
