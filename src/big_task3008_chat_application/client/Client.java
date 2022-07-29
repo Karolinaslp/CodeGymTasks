a54c56d9cc1452ab72285aa5cd0e9a9a374917db
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-07-29 19:10:57 +0200

    add run and main to Client class

--- src/big_task3008_chat_application/client/Client.java
+++ src/big_task3008_chat_application/client/Client.java
@@ -47,4 +47,42 @@ public class Client {
             clientConnected = false;
         }
     }
+    public void run() {
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+
+
+        try {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
+            ConsoleHelper.writeMessage("An error occurred while running the client");
+            return;
+        }
+
+        if (clientConnected)
+            ConsoleHelper.writeMessage("A connection has been established. To exit, enter 'exit'.");
+        else
+            ConsoleHelper.writeMessage("An error occurred while running the client.");
+
+        while (clientConnected) {
+            String text = null;
+            try {
+                text = ConsoleHelper.readString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (text.equalsIgnoreCase("exit"))
+                break;
+
+            if (shouldSendTextFromConsole())
+                sendTextMessage(text);
+        }
+    }
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
 }
