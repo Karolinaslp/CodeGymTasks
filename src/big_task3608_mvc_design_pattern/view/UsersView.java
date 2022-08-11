f8ed24b95e23b9b4aff4968468b5e096ce413548
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-11 22:41:30 +0200

    create and implement UsersView class in view package

---
+++ src/big_task3608_mvc_design_pattern/view/UsersView.java
@@ -0,0 +1,29 @@
+package big_task3608_mvc_design_pattern.view;
+
+import big_task3608_mvc_design_pattern.bean.User;
+import big_task3608_mvc_design_pattern.controller.Controller;
+import big_task3608_mvc_design_pattern.model.DataModel;
+
+public class UsersView implements View {
+    private Controller controller;
+
+    @Override
+    public void refresh(DataModel dataModel) {
+        System.out.println("All users:");
+        for (User user : dataModel.getUsers()) {
+            System.out.println("\t" + user);
+        }
+        System.out.println("===================================================");
+
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireShowAllUsersEvent() {
+        controller.onShowAllUsers();
+    }
+}
+
