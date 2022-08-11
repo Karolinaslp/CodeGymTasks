a9b8316cb240a897594a1fd65fded656b75c76a3
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-11 22:42:30 +0200

    change onShowAllUsers() method, add userView field and setter

--- src/big_task3608_mvc_design_pattern/controller/Controller.java
+++ src/big_task3608_mvc_design_pattern/controller/Controller.java
@@ -1,9 +1,11 @@
 package big_task3608_mvc_design_pattern.controller;
 
 import big_task3608_mvc_design_pattern.model.Model;
+import big_task3608_mvc_design_pattern.view.UsersView;
 
 public class Controller {
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -11,5 +13,10 @@ public class Controller {
 
     public void onShowAllUsers() {
         model.loadUsers();
+        usersView.refresh(model.getDataModel());
+    }
+
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 }
