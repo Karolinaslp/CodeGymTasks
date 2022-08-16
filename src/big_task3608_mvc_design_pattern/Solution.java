78642a51b347ed0cafc45bba5e420e92098b817b
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-16 11:17:41 +0200

    add the ability to open an edit form for the user with id = 126

--- src/big_task3608_mvc_design_pattern/Solution.java
+++ src/big_task3608_mvc_design_pattern/Solution.java
@@ -3,19 +3,26 @@ package big_task3608_mvc_design_pattern;
 import big_task3608_mvc_design_pattern.controller.Controller;
 import big_task3608_mvc_design_pattern.model.MainModel;
 import big_task3608_mvc_design_pattern.model.Model;
+import big_task3608_mvc_design_pattern.view.EditUserView;
 import big_task3608_mvc_design_pattern.view.UsersView;
 
 public class Solution {
     public static void main(String[] args) {
         Model model = new MainModel();
         UsersView usersView = new UsersView();
+        EditUserView editUserView = new EditUserView();
         Controller controller = new Controller();
 
         usersView.setController(controller);
+        editUserView.setController(controller);
+
         controller.setModel(model);
         controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
 
+        //emulate user events
         usersView.fireShowAllUsersEvent();
+        usersView.fireOpenUserEditFormEvent(126);
         usersView.fireShowDeletedUsersEvent();
     }
 }
