6e31561c98455df3662c7fb5fd365bec9a3dc0a7
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-16 10:15:36 +0200

    create activeUser field along with getters and setters

--- src/big_task3608_mvc_design_pattern/model/DataModel.java
+++ src/big_task3608_mvc_design_pattern/model/DataModel.java
@@ -5,7 +5,17 @@ import big_task3608_mvc_design_pattern.bean.User;
 import java.util.List;
 
 public class DataModel {
+    private User activeUser;
     private List<User> users;
+    private boolean displayDeletedUserList;
+
+    public User getActiveUser() {
+        return activeUser;
+    }
+
+    public void setActiveUser(User activeUser) {
+        this.activeUser = activeUser;
+    }
 
     public List<User> getUsers() {
         return users;
@@ -13,5 +23,13 @@ public class DataModel {
 
     public void setUsers(List<User> users) {
         this.users = users;
+    }
+
+    public boolean isDisplayDeletedUserList() {
+        return displayDeletedUserList;
+    }
+
+    public void setDisplayDeletedUserList(boolean displayDeletedUserList) {
+        this.displayDeletedUserList = displayDeletedUserList;
     }
 }
