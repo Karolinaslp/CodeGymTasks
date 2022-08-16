55e4f3f227e4eeb012e31a29691e2c0066137269
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-16 10:19:29 +0200

    add EditUserView field with corresponding getter and setter

--- src/big_task3608_mvc_design_pattern/controller/Controller.java
+++ src/big_task3608_mvc_design_pattern/controller/Controller.java
@@ -1,11 +1,13 @@
 package big_task3608_mvc_design_pattern.controller;
 
 import big_task3608_mvc_design_pattern.model.Model;
+import big_task3608_mvc_design_pattern.view.EditUserView;
 import big_task3608_mvc_design_pattern.view.UsersView;
 
 public class Controller {
     private Model model;
     private UsersView usersView;
+    private EditUserView editUserView;
 
     public void setModel(Model model) {
         this.model = model;
@@ -23,5 +25,9 @@ public class Controller {
     public void onShowAllDeletedUsers() {
         model.loadDeletedUsers();
         usersView.refresh(model.getDataModel());
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
     }
 }
