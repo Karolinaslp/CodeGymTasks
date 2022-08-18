47023f7ea8953329fd9f424b6e204f338b2776c9
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-18 18:39:56 +0200

    add onDeleteUser() and onChangeUserData() methods

--- src/big_task3608_mvc_design_pattern/controller/Controller.java
+++ src/big_task3608_mvc_design_pattern/controller/Controller.java
@@ -13,13 +13,17 @@ public class Controller {
         this.model = model;
     }
 
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
+    }
+
+    public void setEditUserView(EditUserView editUserView) {
+        this.editUserView = editUserView;
+    }
+
     public void onShowAllUsers() {
         model.loadUsers();
         usersView.refresh(model.getDataModel());
-    }
-
-    public void setUsersView(UsersView usersView) {
-        this.usersView = usersView;
     }
 
     public void onShowAllDeletedUsers() {
@@ -27,12 +31,19 @@ public class Controller {
         usersView.refresh(model.getDataModel());
     }
 
-    public void setEditUserView(EditUserView editUserView) {
-        this.editUserView = editUserView;
-    }
-
     public void onOpenUserEditForm(long userId) {
         model.loadUserById(userId);
         editUserView.refresh(model.getDataModel());
     }
+
+    public void onDeleteUser(long id) {
+        model.deleteUserById(id);
+        usersView.refresh(model.getDataModel());
+    }
+
+    public void onChangeUserData(String name, long id, int level) {
+        model.changeUserData(name, id, level);
+        usersView.refresh(model.getDataModel());
+    }
 }
+
