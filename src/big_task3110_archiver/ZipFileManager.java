2e5ae92aae764397d0dd542681d0386f6d4cb25b
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-22 20:17:47 +0200

    create archiver class and add main method to it

---
+++ src/big_task3110_archiver/ZipFileManager.java
@@ -0,0 +1,16 @@
+package big_task3110_archiver;
+
+import java.nio.file.Path;
+
+public class ZipFileManager {
+    //I will use this to store the full path to the archiver
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception{
+
+    }
+}
