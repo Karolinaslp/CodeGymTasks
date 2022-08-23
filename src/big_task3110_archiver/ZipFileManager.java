f98edae855f1a19187c44f2cb24c4c596d77462f
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-23 21:28:06 +0200

    implement createZip() method

--- src/big_task3110_archiver/ZipFileManager.java
+++ src/big_task3110_archiver/ZipFileManager.java
@@ -1,6 +1,10 @@
 package big_task3110_archiver;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     //I will use this to store the full path to the archiver
@@ -10,7 +14,19 @@ public class ZipFileManager {
         this.zipFile = zipFile;
     }
 
-    public void createZip(Path source) throws Exception{
-
+    //This method zip the file specified by the source argument.
+    public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+            ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            try (InputStream fileInputStream = Files.newInputStream(source)) {
+                byte[] buffer = new byte[8 * 1024];
+                int len;
+                while ((len = fileInputStream.read(buffer)) > 0) {
+                    zipOutputStream.write(buffer, 0, len);
+                }
+            }
+            zipOutputStream.closeEntry();
+        }
     }
 }
