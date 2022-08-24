0c9e5905e67fcafc99cb754b12c93532359b5d68
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-08-24 12:25:15 +0200

    declare Operation enum, and add commands

---
+++ src/big_task3110_archiver/Operation.java
@@ -0,0 +1,16 @@
+package big_task3110_archiver;
+
+public enum Operation {
+    //Create an archive
+    CREATE,
+    //Add a file to an archive
+    ADD,
+    //Remove a file from an archive
+    REMOVE,
+    //Extract an archive's contents
+    EXTRACT,
+    //View an archive's contents
+    CONTENT,
+    //Exit the program
+    EXIT,
+}
