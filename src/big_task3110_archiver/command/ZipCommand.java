862693bf3248f4c27eacef6d5c48e305bb522a04
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-06 14:38:38 +0200

    create ZipFileManager getZipFileManager() method

--- src/big_task3110_archiver/command/ZipCommand.java
+++ src/big_task3110_archiver/command/ZipCommand.java
@@ -1,8 +1,22 @@
 package big_task3110_archiver.command;
 
+import big_task3110_archiver.ConsoleHelper;
+import big_task3110_archiver.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipCommand implements Command {
     @Override
     public void execute() throws Exception {
 
+    }
+
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Please enter the full path to the archive");
+        final String pathName = ConsoleHelper.readString();
+        Path path = Paths.get(pathName);
+
+        return new ZipFileManager(path);
     }
 }
