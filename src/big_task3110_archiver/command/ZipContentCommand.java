3c62f010a5007f84adeb3970cc00d155565e3adf
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-12 16:53:21 +0200

    implement ZipContentCommand class execute() method

--- src/big_task3110_archiver/command/ZipContentCommand.java
+++ src/big_task3110_archiver/command/ZipContentCommand.java
@@ -1,4 +1,26 @@
 package big_task3110_archiver.command;
 
+import big_task3110_archiver.ConsoleHelper;
+import big_task3110_archiver.FileProperties;
+import big_task3110_archiver.ZipFileManager;
+
+import java.util.List;
+
 public class ZipContentCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Viewing contents of all archive.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Archive contents:");
+
+        final List<FileProperties> fileList = zipFileManager.getFileList();
+
+        for (FileProperties properties : fileList) {
+            ConsoleHelper.writeMessage(properties.toString());
+        }
+
+        ConsoleHelper.writeMessage("Archive contents viewed");
+    }
 }
