612b99cec599ac5fea1fdc56dda1ea94cf4ba0e0
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-14 21:21:56 +0200

    implement ZipExtractCommand class execute() method

--- src/big_task3110_archiver/command/ZipExtractCommand.java
+++ src/big_task3110_archiver/command/ZipExtractCommand.java
@@ -1,4 +1,28 @@
 package big_task3110_archiver.command;
 
+import big_task3110_archiver.ConsoleHelper;
+import big_task3110_archiver.ZipFileManager;
+import big_task3110_archiver.exception.PathNotFoundException;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipExtractCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+
+        try{
+            ConsoleHelper.writeMessage("Extracting an archive.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Enter the full name of the archive:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(sourcePath);
+
+            ConsoleHelper.writeMessage("File successfully extracted");
+        } catch (PathNotFoundException e) {
+            ConsoleHelper.writeMessage("You didn't correctly enter a file name or directory.");
+        }
+    }
 }
