9a83d058538cd1d288dceb6ebb7dce8a8c6c7b20
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-09-15 13:07:49 +0200

    implement execute() method

--- src/big_task3110_archiver/command/ZipRemoveCommand.java
+++ src/big_task3110_archiver/command/ZipRemoveCommand.java
@@ -1,4 +1,22 @@
 package big_task3110_archiver.command;
 
+import big_task3110_archiver.ConsoleHelper;
+import big_task3110_archiver.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipRemoveCommand extends ZipCommand {
+    @Override
+    public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Removing a file from an archive.");
+
+        ZipFileManager zipfileManager = getZipFileManager();
+
+        ConsoleHelper.writeMessage("Enter the full path to the file in the archive");
+        Path sourcePath = Paths.get(ConsoleHelper.readString());
+        zipfileManager.removeFile(sourcePath);
+
+        ConsoleHelper.writeMessage("File removed from archive.");
+    }
 }
