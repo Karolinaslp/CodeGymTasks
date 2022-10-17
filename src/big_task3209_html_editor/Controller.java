81d1ad340e0787f7da998cd3e9cc1af8d5b0586a
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-10-17 18:16:45 +0200

    initial commit

---
+++ src/big_task3209_html_editor/Controller.java
@@ -0,0 +1,18 @@
+package big_task3209_html_editor;
+
+import javax.swing.text.html.HTMLDocument;
+import java.io.File;
+
+public class Controller {
+    private View view;
+    private HTMLDocument document;
+    private File currentFile;
+
+    public Controller(View view) {
+        this.view = view;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
