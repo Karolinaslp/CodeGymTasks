ae313c489720d35032ca52ef63fe24b2b6accc0d
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-14 15:30:18 +0100

    create UndoAction class

---
+++ src/big_task3209_html_editor/actions/UndoAction.java
@@ -0,0 +1,21 @@
+package big_task3209_html_editor.actions;
+
+
+import big_task3209_html_editor.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class UndoAction extends AbstractAction {
+    View view;
+
+    public UndoAction(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
+
