466ead605001d6c1058ee4664516502b73a68eaa
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-14 15:23:46 +0100

    create UndoMenuListener class

---
+++ src/big_task3209_html_editor/listeners/UndoMenuListener.java
@@ -0,0 +1,27 @@
+package big_task3209_html_editor.listeners;
+
+import big_task3209_html_editor.View;
+
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class UndoMenuListener implements MenuListener {
+    public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+
+    }
+}
