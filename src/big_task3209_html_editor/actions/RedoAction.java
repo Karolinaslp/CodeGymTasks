8691d2f1b5201fccb903c4db4017b47a121964d7
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-14 15:20:05 +0100

    create RedoAction class

---
+++ src/big_task3209_html_editor/actions/RedoAction.java
@@ -0,0 +1,19 @@
+package big_task3209_html_editor.actions;
+
+import big_task3209_html_editor.View;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+public class RedoAction extends AbstractAction {
+    View view;
+
+    public RedoAction(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
