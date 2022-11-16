a9fabc8e9a9f3495ada4b261bf73e8ba2753ac02
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-16 12:41:51 +0100

    add access modifier

--- src/big_task3209_html_editor/actions/UndoAction.java
+++ src/big_task3209_html_editor/actions/UndoAction.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class UndoAction extends AbstractAction {
-    View view;
+    private final View view;
 
     public UndoAction(View view) {
         this.view = view;
