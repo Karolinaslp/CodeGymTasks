89673559a566a582972181a043371f817e51eb1e
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-24 17:02:32 +0100

    add createOrder() method

--- src/big_task2712_restaurant_manager/Tablet.java
+++ src/big_task2712_restaurant_manager/Tablet.java
@@ -1,4 +1,14 @@
 package big_task2712_restaurant_manager;
 
 public class Tablet {
+    //Tablet number
+    private final int number;
+
+    public Tablet(int number) {
+        this.number = number;
+    }
+    //Creates an order consisting of the dishes chosen by the customer.
+    public void createOrder() {
+
+    }
 }
