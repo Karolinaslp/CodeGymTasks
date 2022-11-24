c74c21dec3950108f02de87418934d8035529dd9
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-24 16:59:43 +0100

    create Dish enum

---
+++ src/big_task2712_restaurant_manager/kitchen/Dish.java
@@ -0,0 +1,9 @@
+package big_task2712_restaurant_manager.kitchen;
+
+public enum Dish {
+    Fish, Steak, Soup, Juice, Water;
+
+    public static String allDishesToString() {
+        return ("Menu: \nFish\nSteak\nSoup\nJuice\nWater");
+    }
+}
