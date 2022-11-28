43253cbd9207df7b6f4747bab4c549a8bda3bba3
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-28 19:58:44 +0100

    implement createOrder(), override toString()

--- src/big_task2712_restaurant_manager/Tablet.java
+++ src/big_task2712_restaurant_manager/Tablet.java
@@ -1,14 +1,34 @@
 package big_task2712_restaurant_manager;
 
+import big_task2712_restaurant_manager.kitchen.Order;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Tablet {
     //Tablet number
     private final int number;
+    private static final Logger logger = Logger.getLogger(Tablet.class.getName());
 
     public Tablet(int number) {
         this.number = number;
     }
+
     //Creates an order consisting of the dishes chosen by the customer.
     public void createOrder() {
+        Order order = null;
+        try {
+            order = new Order(this);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "The console is unavailable.");
+        }
+    }
 
+    @Override
+    public String toString() {
+        return "Tablet{" +
+                "number=" + number +
+                '}';
     }
 }
