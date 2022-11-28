02324d893bcc59adb5e8cfe8806e870c3c480732
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-28 19:57:46 +0100

    add code to test application

--- src/big_task2712_restaurant_manager/Restaurant.java
+++ src/big_task2712_restaurant_manager/Restaurant.java
@@ -2,6 +2,10 @@ package big_task2712_restaurant_manager;
 
 public class Restaurant {
     public static void main(String[] args) {
-
+        Tablet tablet = new Tablet(1);
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
     }
 }
