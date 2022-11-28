9d84c6691f2decb29520a7fcfb9602ef716e1edf
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-11-28 19:57:31 +0100

    initialize dishes list in the constructor, override toString()

--- src/big_task2712_restaurant_manager/kitchen/Order.java
+++ src/big_task2712_restaurant_manager/kitchen/Order.java
@@ -1,14 +1,30 @@
 package big_task2712_restaurant_manager.kitchen;
 
+import big_task2712_restaurant_manager.ConsoleHelper;
 import big_task2712_restaurant_manager.Tablet;
 
+import java.io.IOException;
 import java.util.List;
 
 public class Order {
     private final Tablet tablet;
     protected List<Dish> dishes;
 
-    public Order(Tablet tablet) {
+    public Order(Tablet tablet) throws IOException {
         this.tablet = tablet;
+        this.dishes = ConsoleHelper.getAllDishesForOrder();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        if (dishes.isEmpty()) return result.toString();
+
+        result.append("Your order: [").append(dishes.get(0));
+        for (int i = 1; i < dishes.size(); i++) {
+            result.append(", ").append(dishes.get(i).name());
+        }
+        result.append("] from ").append(tablet);
+        return result.toString();
     }
 }
