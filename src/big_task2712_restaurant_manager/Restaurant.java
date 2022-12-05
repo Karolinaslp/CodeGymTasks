543affa9697f5a750c5d88a51b206942b4ccfe99
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-05 13:10:12 +0100

    add waiter object to test app

--- src/big_task2712_restaurant_manager/Restaurant.java
+++ src/big_task2712_restaurant_manager/Restaurant.java
@@ -1,16 +1,16 @@
 package big_task2712_restaurant_manager;
 
 import big_task2712_restaurant_manager.kitchen.Cook;
+import big_task2712_restaurant_manager.kitchen.Waiter;
 
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(1);
         final Cook cook = new Cook("Adam");
+        final Waiter waiter = new Waiter();
         tablet.addObserver(cook);
+        cook.addObserver(waiter);
 
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
         tablet.createOrder();
     }
 }
