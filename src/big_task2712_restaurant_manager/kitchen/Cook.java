3b369403278f2ebb76b70d1112b8ea19f5a73a1b
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-05 12:23:30 +0100

    implement Observer interface

--- src/big_task2712_restaurant_manager/kitchen/Cook.java
+++ src/big_task2712_restaurant_manager/kitchen/Cook.java
@@ -1,4 +1,25 @@
 package big_task2712_restaurant_manager.kitchen;
 
-public class Cook {
+import big_task2712_restaurant_manager.ConsoleHelper;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class Cook implements Observer {
+    private final String name;
+
+    public Cook(String cookName) {
+        this.name = cookName;
+    }
+
+    @Override
+    public void update(Observable o, Object order) {
+        ConsoleHelper.writeMassage("Start cooking - " + order);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
+
