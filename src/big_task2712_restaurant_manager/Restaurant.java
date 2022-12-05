f0a632a0fcc4445f8e1592232852089d525a6df1
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-05 12:24:46 +0100

    test addObserver() method

--- src/big_task2712_restaurant_manager/Restaurant.java
+++ src/big_task2712_restaurant_manager/Restaurant.java
@@ -1,8 +1,13 @@
 package big_task2712_restaurant_manager;
 
+import big_task2712_restaurant_manager.kitchen.Cook;
+
 public class Restaurant {
     public static void main(String[] args) {
         Tablet tablet = new Tablet(1);
+        final Cook cook = new Cook("Adam");
+        tablet.addObserver(cook);
+
         tablet.createOrder();
         tablet.createOrder();
         tablet.createOrder();
