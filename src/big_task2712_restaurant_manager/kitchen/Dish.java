a3376d7c5c7bc55add20a9ca081618e1697f8388
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-06 11:30:49 +0100

    add duration field, constructor and getter

--- src/big_task2712_restaurant_manager/kitchen/Dish.java
+++ src/big_task2712_restaurant_manager/kitchen/Dish.java
@@ -1,7 +1,17 @@
 package big_task2712_restaurant_manager.kitchen;
 
 public enum Dish {
-    Fish, Steak, Soup, Juice, Water;
+    Fish(25), Steak(30), Soup(15), Juice(5), Water(3);
+
+    private final int duration;
+
+    Dish(int duration) {
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
 
     public static String allDishesToString() {
         return ("Menu: \nFish\nSteak\nSoup\nJuice\nWater");
