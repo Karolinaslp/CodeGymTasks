7e20a5de0659fe06acc4cde9c198a0d0ba8bfa72
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-12 11:25:01 +0100

    add processVideos() method

--- src/big_task2712_restaurant_manager/add/AdvertisementManager.java
+++ src/big_task2712_restaurant_manager/add/AdvertisementManager.java
@@ -1,5 +1,16 @@
 package big_task2712_restaurant_manager.add;
 
+import big_task2712_restaurant_manager.ConsoleHelper;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage = AdvertisementStorage.getInstance();
+    private final int timeSeconds;
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+
+    public void processVideos() {
+        ConsoleHelper.writeMassage("calling the processVideos method");
+    }
 }
