9b615581fec067ff03af78325ebc08f1f4c3bdfc
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-12 11:20:17 +0100

    add getDuration() and getAmountPerImpression() method

--- src/big_task2712_restaurant_manager/add/Advertisement.java
+++ src/big_task2712_restaurant_manager/add/Advertisement.java
@@ -1,11 +1,13 @@
 package big_task2712_restaurant_manager.add;
 
 public class Advertisement {
-    private Object content;
-    private String name;
-    private long amountPaid;
-    private int impressionsRemaining;
-    private int duration;
+    private final Object content;
+    private final String name;
+    private final long amountPaid;
+    private final int impressionsRemaining;
+    private final int duration;
+    private final long amountPerImpression;
+
 
     public Advertisement(Object content, String name, long amountPaid, int impressionsRemaining, int duration) {
         this.content = content;
@@ -13,6 +15,19 @@ public class Advertisement {
         this.amountPaid = amountPaid;
         this.impressionsRemaining = impressionsRemaining;
         this.duration = duration;
+        amountPerImpression = amountPaid / impressionsRemaining;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public long getAmountPerImpression() {
+        return amountPerImpression;
     }
 }
 
