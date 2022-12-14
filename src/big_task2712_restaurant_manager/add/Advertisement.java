33992aacaf1775f20a270d0f4e38bd366d351cf3
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-14 17:00:48 +0100

    add revalidate() method

--- src/big_task2712_restaurant_manager/add/Advertisement.java
+++ src/big_task2712_restaurant_manager/add/Advertisement.java
@@ -1,12 +1,21 @@
 package big_task2712_restaurant_manager.add;
 
 public class Advertisement {
+    //The video.
     private final Object content;
+
     private final String name;
+
+    //The amount paid for ads in cents.
     private final long amountPaid;
-    private final int impressionsRemaining;
+
+    //The number of impressions that have been paid for but not shown.
+    private int impressionsRemaining;
+
+    //The duration in seconds.
     private final int duration;
-    private final long amountPerImpression;
+
+    private long amountPerImpression;
 
 
     public Advertisement(Object content, String name, long amountPaid, int impressionsRemaining, int duration) {
@@ -15,7 +24,9 @@ public class Advertisement {
         this.amountPaid = amountPaid;
         this.impressionsRemaining = impressionsRemaining;
         this.duration = duration;
-        amountPerImpression = amountPaid / impressionsRemaining;
+        if (impressionsRemaining > 0) {
+            amountPerImpression = amountPaid / impressionsRemaining;
+        }
     }
 
     public String getName() {
@@ -28,6 +39,13 @@ public class Advertisement {
 
     public long getAmountPerImpression() {
         return amountPerImpression;
+    }
+
+    public void revalidate() {
+        if (impressionsRemaining == 0) {
+            throw new UnsupportedOperationException();
+        }
+        impressionsRemaining--;
     }
 }
 
