347a114f1ac4d3cc4326db874368b0a4edc02a78
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-15 10:59:19 +0100

    create VideosSelectedEventDataRow class

---
+++ src/big_task2712_restaurant_manager/statistics/event/VideosSelectedEventDataRow.java
@@ -0,0 +1,27 @@
+package big_task2712_restaurant_manager.statistics.event;
+
+import big_task2712_restaurant_manager.add.Advertisement;
+
+import java.util.Date;
+import java.util.List;
+
+public class VideosSelectedEventDataRow implements EventDataRow {
+    //The amount of money in cents
+    private final long amount;
+
+    //The list of videos chosen for playback
+    private final List<Advertisement> optimalVideoSet;
+
+    //the total duration of the chosen commercials
+    private final int totalDuration;
+
+    private final Date currentDate;
+
+    public VideosSelectedEventDataRow(List<Advertisement> optimalVideoSet, long amount, int totalDuration) {
+        this.optimalVideoSet = optimalVideoSet;
+        this.amount = amount;
+        this.totalDuration = totalDuration;
+        this.currentDate = new Date();
+    }
+}
+
