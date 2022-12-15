38f49791f1d60f36e8dba0be95746bf81428eab2
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-15 10:58:47 +0100

    create OrderReadyEventDataRow class

---
+++ src/big_task2712_restaurant_manager/statistics/event/OrderReadyEventDataRow.java
@@ -0,0 +1,30 @@
+package big_task2712_restaurant_manager.statistics.event;
+
+import big_task2712_restaurant_manager.kitchen.Dish;
+
+import java.util.Date;
+import java.util.List;
+
+public class OrderReadyEventDataRow implements EventDataRow{
+    //Name of the tablet
+    private final String tabletName;
+
+    //Name of the cook
+    private final String cookName;
+
+    //Time required to prepare the order in seconds
+    private final int cookingTimeSeconds;
+
+    //The list of dishes to prepare
+    private final List<Dish> dishesInOrder;
+
+    private final Date currentDate;
+
+    public OrderReadyEventDataRow(String tabletName, String cookName, int cookingTimeSeconds, List<Dish> dishesInOrder) {
+        this.tabletName = tabletName;
+        this.cookName = cookName;
+        this.cookingTimeSeconds = cookingTimeSeconds;
+        this.dishesInOrder = dishesInOrder;
+        this.currentDate = new Date();
+    }
+}
