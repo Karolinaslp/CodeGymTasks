346e2b461fe8da90cc20816c0d9ecd7ba65d5e5c
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-16 13:01:44 +0100

    generate appropriate event in update() method

--- src/big_task2712_restaurant_manager/kitchen/Cook.java
+++ src/big_task2712_restaurant_manager/kitchen/Cook.java
@@ -1,6 +1,8 @@
 package big_task2712_restaurant_manager.kitchen;
 
 import big_task2712_restaurant_manager.ConsoleHelper;
+import big_task2712_restaurant_manager.statistics.StatisticsManager;
+import big_task2712_restaurant_manager.statistics.event.OrderReadyEventDataRow;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -13,10 +15,13 @@ public class Cook extends Observable implements Observer {
     }
 
     @Override
-    public void update(Observable o, Object order) {
-        ConsoleHelper.writeMassage("Start cooking - " + order);
+    public void update(Observable o, Object arg) {
+        Order order = (Order) arg;
+        ConsoleHelper.writeMessage("Start cooking - " + order);
         setChanged();
         notifyObservers(order);
+        OrderReadyEventDataRow row = new OrderReadyEventDataRow(order.getTablet().toString(), name, order.getTotalCookingTime() * 60, order.getDishes());
+        StatisticsManager.getInstance().record(row);
     }
 
     @Override
