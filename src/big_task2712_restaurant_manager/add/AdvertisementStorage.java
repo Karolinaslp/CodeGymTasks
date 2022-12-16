0c7c41a2c2b987abc84e695b3be9e305bac6b4f5
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-16 12:58:47 +0100

    change ArrayList to LinkedList in videos filed

--- src/big_task2712_restaurant_manager/add/AdvertisementStorage.java
+++ src/big_task2712_restaurant_manager/add/AdvertisementStorage.java
@@ -1,6 +1,6 @@
 package big_task2712_restaurant_manager.add;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class AdvertisementStorage {
@@ -12,7 +12,7 @@ public class AdvertisementStorage {
         return InstanceHolder.instance;
     }
 
-    private final List<Advertisement> videos = new ArrayList<>();
+    private final List<Advertisement> videos = new LinkedList<>();
 
     private AdvertisementStorage() {
         Object someContent = new Object();
