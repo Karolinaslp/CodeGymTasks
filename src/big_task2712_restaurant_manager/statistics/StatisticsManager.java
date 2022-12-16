f6bc4242ebe7eefb19f2327cb64940f00a760ba1
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-16 12:03:59 +0100

    creating repository in StatisticsManager class

--- src/big_task2712_restaurant_manager/statistics/StatisticsManager.java
+++ src/big_task2712_restaurant_manager/statistics/StatisticsManager.java
@@ -1,21 +1,35 @@
 package big_task2712_restaurant_manager.statistics;
 
 import big_task2712_restaurant_manager.statistics.event.EventDataRow;
+import big_task2712_restaurant_manager.statistics.event.EventType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StatisticsManager {
-    private static class InstanceHolder {
-        private static final StatisticsManager instance = new StatisticsManager();
-    }
+    private final static StatisticsManager ourInstance = new StatisticsManager();
 
     public static StatisticsManager getInstance() {
-        return InstanceHolder.instance;
+        return ourInstance;
     }
 
+    private final StatisticsStorage statisticsStorage = new StatisticsStorage();
+
     private StatisticsManager() {
-        Object someStatistic = new Object();
     }
 
     public void record(EventDataRow dataRow) {
+    }
 
+    private static class StatisticsStorage {
+        private final Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+
+        public StatisticsStorage() {
+            for (EventType type : EventType.values()) {
+                this.storage.put(type, new ArrayList<>());
+            }
+        }
     }
 }
