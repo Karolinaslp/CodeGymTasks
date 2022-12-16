b8b10d10755c17f136f24419eb773ac1e0a6ed83
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-16 13:06:20 +0100

    implement put() method

--- src/big_task2712_restaurant_manager/statistics/StatisticsManager.java
+++ src/big_task2712_restaurant_manager/statistics/StatisticsManager.java
@@ -9,27 +9,36 @@ import java.util.List;
 import java.util.Map;
 
 public class StatisticsManager {
-    private final static StatisticsManager ourInstance = new StatisticsManager();
+    private static StatisticsManager ourInstance = new StatisticsManager();
 
     public static StatisticsManager getInstance() {
         return ourInstance;
     }
 
-    private final StatisticsStorage statisticsStorage = new StatisticsStorage();
+    private StatisticsStorage statisticsStorage = new StatisticsStorage();
 
     private StatisticsManager() {
     }
 
-    public void record(EventDataRow dataRow) {
-    }
+    private class StatisticsStorage {
+        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
 
-    private static class StatisticsStorage {
-        private final Map<EventType, List<EventDataRow>> storage = new HashMap<>();
-
-        public StatisticsStorage() {
+        private StatisticsStorage() {
             for (EventType type : EventType.values()) {
-                this.storage.put(type, new ArrayList<>());
+                this.storage.put(type, new ArrayList<EventDataRow>());
             }
         }
+
+        private void put(EventDataRow data) {
+            EventType type = data.getType();
+            if (!this.storage.containsKey(type))
+                throw new UnsupportedOperationException();
+
+            this.storage.get(type).add(data);
+        }
+    }
+
+    public void record(EventDataRow data) {
+        this.statisticsStorage.put(data);
     }
 }
