679062f58843e469f52bd7d07495b7f6aef8be3f
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-19 17:32:26 +0100

    creating Date getDate(), which returns the row's creation date and int getTime(), which returns the duration

--- src/big_task2712_restaurant_manager/statistics/event/EventDataRow.java
+++ src/big_task2712_restaurant_manager/statistics/event/EventDataRow.java
@@ -1,5 +1,9 @@
 package big_task2712_restaurant_manager.statistics.event;
 
+import java.util.Date;
+
 public interface EventDataRow {
     EventType getType();
+    Date getDate();
+    int getTime();
 }
