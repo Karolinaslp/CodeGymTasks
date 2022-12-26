7cfc7e5abc7f63fcf5b3c821bf6867645b45a2ed
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-26 18:17:01 +0100

    add createTestOrder() and processOrder() methods

--- src/big_task2712_restaurant_manager/Tablet.java
+++ src/big_task2712_restaurant_manager/Tablet.java
@@ -3,22 +3,23 @@ package big_task2712_restaurant_manager;
 import big_task2712_restaurant_manager.add.AdvertisementManager;
 import big_task2712_restaurant_manager.add.NoVideoAvailableException;
 import big_task2712_restaurant_manager.kitchen.Order;
+import big_task2712_restaurant_manager.kitchen.TestOrder;
 
 import java.io.IOException;
 import java.util.Observable;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Tablet extends Observable {
-    //Tablet number
     private final int number;
     private static final Logger logger = Logger.getLogger(Tablet.class.getName());
+    private LinkedBlockingQueue<Order> queue;
 
     public Tablet(int number) {
         this.number = number;
     }
 
-    //Creates an order consisting of the dishes chosen by the customer.
     public Order createOrder() {
         Order order = null;
         try {
@@ -32,16 +33,43 @@ public class Tablet extends Observable {
             notifyObservers(order);
         } catch (IOException e) {
             logger.log(Level.SEVERE, "The console is unavailable.");
-        } catch (NoVideoAvailableException e) {
+        } catch (NoVideoAvailableException nve) {
             logger.log(Level.INFO, "No video is available for the following order: " + order);
         }
         return order;
     }
 
-    @Override
+    private boolean processOrder(Order order) {
+        ConsoleHelper.writeMessage(order.toString());
+        if (order.isEmpty()) {
+            return true;
+        }
+        setChanged();
+        notifyObservers(order);
+
+        new AdvertisementManager(order.getTotalCookingTime() * 60).processVideos();
+        return false;
+    }
+
+    public void createTestOrder() {
+        Order order = null;
+        try {
+            order = new TestOrder(this);
+            processOrder(order);
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "The console is unavailable");
+        } catch (NoVideoAvailableException e) {
+            logger.log(Level.INFO, "No video is available foe the following order: " + order);
+        }
+    }
+
     public String toString() {
         return "Tablet{" +
                 "number=" + number +
                 '}';
+    }
+
+    public void setQueue(LinkedBlockingQueue<Order> queue) {
+        this.queue = queue;
     }
 }
