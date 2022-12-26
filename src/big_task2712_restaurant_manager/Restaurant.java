e201591e833283832d7440f08c447dec9776be4e
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-26 18:15:30 +0100

    add constant ORDER_CREATION_INTERVAL

--- src/big_task2712_restaurant_manager/Restaurant.java
+++ src/big_task2712_restaurant_manager/Restaurant.java
@@ -4,13 +4,24 @@ import big_task2712_restaurant_manager.kitchen.Cook;
 import big_task2712_restaurant_manager.kitchen.Waiter;
 
 public class Restaurant {
+    private static final int ORDER_CREATION_INTERVAL = 100;
+
     public static void main(String[] args) {
-        Tablet tablet = new Tablet(1);
-        final Cook cook = new Cook("Adam");
-        final Waiter waiter = new Waiter();
+        Tablet tablet = new Tablet(5);
+        Cook cook = new Cook("Amigo");
+        Waiter waiter = new Waiter();
         tablet.addObserver(cook);
         cook.addObserver(waiter);
 
         tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
+        tablet.createOrder();
+
+        ManagerTablet managerTablet = new ManagerTablet();
+        managerTablet.printAdRevenue();
+        managerTablet.printCookUtilization();
+        managerTablet.printActiveVideoSet();
+        managerTablet.printArchivedVideoSet();
     }
 }
