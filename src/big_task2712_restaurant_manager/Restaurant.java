d38bb39e2880c7e9c98babac669fe9af814fda09
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-27 12:44:30 +0100

    refactor main method. to test the multithreading order generating

--- src/big_task2712_restaurant_manager/Restaurant.java
+++ src/big_task2712_restaurant_manager/Restaurant.java
@@ -1,22 +1,40 @@
 package big_task2712_restaurant_manager;
 
 import big_task2712_restaurant_manager.kitchen.Cook;
-import big_task2712_restaurant_manager.kitchen.Waiter;
+import big_task2712_restaurant_manager.statistics.StatisticsManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Restaurant {
     private static final int ORDER_CREATION_INTERVAL = 100;
 
     public static void main(String[] args) {
-        Tablet tablet = new Tablet(5);
-        Cook cook = new Cook("Amigo");
-        Waiter waiter = new Waiter();
-        tablet.addObserver(cook);
-        cook.addObserver(waiter);
+        Cook cookMichal = new Cook("Michal");
+        Cook cookKarolina = new Cook("Karolina");
 
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
-        tablet.createOrder();
+        List<Tablet> tablets = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            Tablet tablet = new Tablet(i);
+            tablet.addObserver(cookMichal);
+            tablet.addObserver(cookKarolina);
+            tablets.add(tablet);
+        }
+
+        StatisticsManager.getInstance().register(cookMichal);
+        StatisticsManager.getInstance().register(cookKarolina);
+
+        Thread thread = new Thread(new RandomOrderGeneratorTask(tablets, ORDER_CREATION_INTERVAL));
+        thread.start();
+
+        try {
+            Thread.sleep(1000);
+            thread.interrupt();
+            thread.join();
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         ManagerTablet managerTablet = new ManagerTablet();
         managerTablet.printAdRevenue();
