cb2d224385c85aacf1cd2d45d534c5b6cc55dd1a
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2022-12-28 11:17:36 +0100

    ad getter to cooks filed

--- src/big_task2712_restaurant_manager/statistics/StatisticsManager.java
+++ src/big_task2712_restaurant_manager/statistics/StatisticsManager.java
@@ -10,24 +10,24 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class StatisticsManager {
-    private static StatisticsManager ourInstance = new StatisticsManager();
+    private final static StatisticsManager ourInstance = new StatisticsManager();
 
     public static StatisticsManager getInstance() {
         return ourInstance;
     }
 
-    private StatisticsStorage statisticsStorage = new StatisticsStorage();
-    private Set<Cook> cooks = new HashSet<>();
+    private final StatisticsStorage statisticsStorage = new StatisticsStorage();
+    private final Set<Cook> cooks = new HashSet<>();
 
     private StatisticsManager() {
     }
 
-    private class StatisticsStorage {
-        private Map<EventType, List<EventDataRow>> storage = new HashMap<>();
+    private static class StatisticsStorage {
+        private final Map<EventType, List<EventDataRow>> storage = new HashMap<>();
 
         private StatisticsStorage() {
             for (EventType type : EventType.values()) {
-                this.storage.put(type, new ArrayList<EventDataRow>());
+                this.storage.put(type, new ArrayList<>());
             }
         }
 
@@ -47,6 +47,10 @@ public class StatisticsManager {
         }
     }
 
+    public Set<Cook> getCooks() {
+        return cooks;
+    }
+
     public void record(EventDataRow data) {
         this.statisticsStorage.put(data);
     }
@@ -59,12 +63,12 @@ public class StatisticsManager {
         Map<String, Long> res = new HashMap();
         List<EventDataRow> rows = statisticsStorage.get(EventType.VIDEOS_SELECTED);
         SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy", Locale.ENGLISH);
-        long total = 0l;
+        long total = 0L;
         for (EventDataRow row : rows) {
             VideosSelectedEventDataRow dataRow = (VideosSelectedEventDataRow) row;
             String date = format.format(dataRow.getDate());
             if (!res.containsKey(date)) {
-                res.put(date, 0l);
+                res.put(date, 0L);
             }
             total += dataRow.getAmount();
             res.put(date, res.get(date) + dataRow.getAmount());
@@ -83,7 +87,7 @@ public class StatisticsManager {
             OrderReadyEventDataRow dataRow = (OrderReadyEventDataRow) row;
             String date = format.format(dataRow.getDate());
             if (!res.containsKey(date)) {
-                res.put(date, new HashMap<String, Integer>());
+                res.put(date, new HashMap<>());
             }
             Map<String, Integer> cookMap = res.get(date);
             String cookName = dataRow.getCookName();
