83ba37f2a106c9341e4f079650937f673c885cdc
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-16 17:58:16 +0100

    made class child of the KayAdapter class. It will be used to handle user input from the keyboard.

--- src/big_task3513_2048_game/Controller.java
+++ src/big_task3513_2048_game/Controller.java
@@ -2,4 +2,14 @@ package big_task3513_2048_game;
 
 // Monitor keystrokes during the game
 public class Controller {
+    private Model model;
+    private View view;
+
+    public Tile[][] getGameTiles() {
+        return model.getGameTiles();
+    }
+
+    public int getScore() {
+        return model.score;
+    }
 }
