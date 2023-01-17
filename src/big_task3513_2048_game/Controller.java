f0b1d9f8c8fb96d0b7ad758a6efb1e9455109058
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-17 21:48:12 +0100

    Added resetGame() for returning the game board to its initial state.

--- src/big_task3513_2048_game/Controller.java
+++ src/big_task3513_2048_game/Controller.java
@@ -1,9 +1,14 @@
 package big_task3513_2048_game;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 // Monitor keystrokes during the game
-public class Controller {
-    private Model model;
-    private View view;
+public class Controller extends KeyAdapter {
+    private final Model model;
+    private final View view;
+
+    private static final int WINNING_TILE = 2048;
 
     public Tile[][] getGameTiles() {
         return model.getGameTiles();
@@ -12,4 +17,40 @@ public class Controller {
     public int getScore() {
         return model.score;
     }
+
+    public Controller(Model model) {
+        this.model = model;
+        view = new View(this);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            resetGame();
+        }
+        if (!model.canMove()) {
+            view.isGameLost = true;
+        }
+        if (!view.isGameLost && !view.isGameWon) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT -> model.left();
+                case KeyEvent.VK_RIGHT -> model.right();
+                case KeyEvent.VK_UP -> model.up();
+                case KeyEvent.VK_DOWN -> model.down();
+            }
+        }
+        if (WINNING_TILE == model.maxTile) {
+            view.isGameWon = true;
+        }
+        view.repaint();
+    }
+
+    public void resetGame() {
+        model.score = 0;
+        view.isGameLost = false;
+        view.isGameWon = false;
+        model.resetGameTiles();
+    }
 }
+
