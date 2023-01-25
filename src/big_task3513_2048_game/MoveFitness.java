f1240848ce708989e8a3bd1c79aff0ccff054edf
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-01-25 21:47:53 +0100

    implemented Comparable interface and override compareTo() method to compare quality of this and previous move

--- src/big_task3513_2048_game/MoveFitness.java
+++ src/big_task3513_2048_game/MoveFitness.java
@@ -1,6 +1,6 @@
 package big_task3513_2048_game;
 
-public class MoveFitness {
+public class MoveFitness implements Comparable<MoveFitness>{
     private final int numberOfEmptyTiles;
     private final int score;
     private final Move move;
@@ -13,5 +13,21 @@ public class MoveFitness {
 
     public Move getMove() {
         return move;
+    }
+
+    @Override
+    public int compareTo(MoveFitness o) {
+        if (numberOfEmptyTiles > o.numberOfEmptyTiles){
+            return  1;
+        } else if (numberOfEmptyTiles < o.numberOfEmptyTiles) {
+            return  -1;
+        } else {
+            if (score > o.score) {
+                return 1;
+            } else if (score < o.score) {
+                return -1;
+            }
+        }
+        return 0;
     }
 }
