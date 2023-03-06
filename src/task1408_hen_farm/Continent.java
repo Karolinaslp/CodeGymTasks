39d643d15faa6710df2c5680838fa99644a8bd17
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-03-06 15:31:12 +0100

    delete unrepresentative tasks

--- src/task1408_hen_farm/Continent.java
+++
@@ -1,8 +0,0 @@
-package task1408_hen_farm;
-
-public interface Continent {
-    String EUROPE = "Europa";
-    String NORTHAMERICA = "Ameryka Północna";
-    String ASIA = "Azja";
-    String AFRICA = "Afryka";
-}
