dea031e37aa38c39a207d927b52ed71118ccb868
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-04-05 20:38:17 +0200

    Tetris initial commit

---
+++ src/big_task2213_tetris/GamePiece.java
@@ -0,0 +1,4 @@
+package big_task2213_tetris;
+
+public class GamePiece {
+}
---
+++ src/big_task2213_tetris/Tetris.java
@@ -0,0 +1,7 @@
+package big_task2213_tetris;
+
+public class Tetris {
+    public static void main(String[] args) {
+
+    }
+}
