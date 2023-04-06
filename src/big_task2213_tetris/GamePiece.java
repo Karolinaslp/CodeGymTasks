2ae25f6937e6c470a4d6de10915d0f46975cb012
Karolina Ślemp-Rejowska <slempkarolina@gmail.com>
2023-04-06 10:03:49 +0200

    Implement logic in Field

--- src/big_task2213_tetris/GamePiece.java
+++ src/big_task2213_tetris/GamePiece.java
@@ -1,4 +1,59 @@
 package big_task2213_tetris;
 
 public class GamePiece {
+    private final int x;
+    private final int y;
+
+    private final int[][] matrix;
+
+    public GamePiece(int x, int y, int[][] matrix) {
+        this.x = x;
+        this.y = y;
+        this.matrix = matrix;
+    }
+
+    public void left() {
+
+    }
+
+    public void right() {
+
+    }
+
+    public void down() {
+
+    }
+
+    public void up() {
+
+    }
+
+    public void rotate() {
+
+    }
+
+    public void downMaximum() {
+
+    }
+
+    public boolean isCurrentPositionAvailable() {
+        return true;
+    }
+
+    public void land() {
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
+
